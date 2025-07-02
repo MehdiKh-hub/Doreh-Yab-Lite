3b112d43fa53e11e86d363969ec6b11132dc9de9
Mohammad Mehdi Khodabande <mmahdikhodabande@gmail.com>
2025-07-02 09:38:57 +0330

    Add Course class to Domain.Entities namespace
    Introduced a new `Course` class with properties for
    `Id`, `Title`, `Description`, `DurationInHours`,
    `Rank`, `SubjectId`, `SourceId`, `TeacherId`, and
    navigation properties for `Subject`, `Source`, and
    `Teacher`. Added a private list for `StudentCourse`
    and a read-only collection. Implemented a public
    constructor for property initialization and an
    `Update` method for modifying course details.

---
+++ Domain/Entities/Course.cs
@@ -0,0 +1,45 @@
+﻿namespace Domain.Entities
+{
+    public class Course
+    {
+        public int Id { get; private set; }
+
+        public string Title { get; private set; }
+        public string? Description { get; private set; }
+        public int DurationInHours { get; private set; }
+        public int Rank { get; private set; }
+
+        public int SubjectId { get; private set; }
+        public Subject Subject { get; private set; }
+
+        public int SourceId { get; private set; }
+        public Source Source { get; private set; }
+
+        public int TeacherId { get; private set; }
+        public Teacher Teacher { get; private set; }
+
+        private readonly List<StudentCourse> _studentCourses = new();
+        public IReadOnlyCollection<StudentCourse> StudentCourses => _studentCourses;
+
+        private Course() { }
+
+        public Course(string title, int duration, int rank, int subjectId, int sourceId, int teacherId, string? description = null)
+        {
+            Title = title;
+            DurationInHours = duration;
+            Rank = rank;
+            SubjectId = subjectId;
+            SourceId = sourceId;
+            TeacherId = teacherId;
+            Description = description;
+        }
+
+        public void Update(string title, int duration, int rank, string? description = null)
+        {
+            Title = title;
+            DurationInHours = duration;
+            Rank = rank;
+            Description = description;
+        }
+    }
+}
