ebe1da1019595c6b07b6493b22cbdd4c1c81452f
Mohammad Mehdi Khodabande <mmahdikhodabande@gmail.com>
2025-07-02 09:57:05 +0330

    Add entity configuration classes for EF Core
    Introduced configuration classes for `Course`, `Source`, `Student`, `StudentCourse`, and `Teacher` entities. Each class implements `IEntityTypeConfiguration<T>` to define table names, primary keys, required properties, and relationships using the Entity Framework Core fluent API. Navigation properties are accessed via fields for improved performance and encapsulation.

---
+++ Infrastracture/Persistence/Configurations/CourseConfig.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastracture.Persistence.Configurations
+{
+    public class CourseConfig : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.ToTable("Courses");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(1000);
+
+            builder.Property(c => c.DurationInHours)
+                .IsRequired();
+
+            builder.Property(c => c.Rank)
+                .IsRequired();
+
+          
+
+            builder.HasOne(c => c.Subject)
+                .WithMany(s => s.Courses)
+                .HasForeignKey(c => c.SubjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(c => c.Source)
+                .WithMany(s => s.Courses)
+                .HasForeignKey(c => c.SourceId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(c => c.Teacher)
+                .WithMany(t => t.Courses)
+                .HasForeignKey(c => c.TeacherId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Metadata.FindNavigation(nameof(Course.StudentCourses))!
+                .SetPropertyAccessMode(PropertyAccessMode.Field);
+        }
+    }
+}
