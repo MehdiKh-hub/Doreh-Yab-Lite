407165e14b973503ea6b592f101aab1f65cba1d7
Mohammad Mehdi Khodabande <mmahdikhodabande@gmail.com>
2025-07-02 09:39:36 +0330

    Add ApplicationDbContext for Entity Framework setup
    This commit introduces the `ApplicationDbContext` class in the `Infrastracture.Persistence` namespace, inheriting from `DbContext`. It defines `DbSet` properties for `Subjects`, `Sources`, `Teachers`, `Courses`, `Students`, and `StudentCourses`. The `OnModelCreating` method is overridden to apply configurations from the assembly, facilitating entity configuration management in Entity Framework Core.

---
+++ Infrastracture/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastracture.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Subject> Subjects => Set<Subject>();
+        public DbSet<Source> Sources => Set<Source>();
+        public DbSet<Teacher> Teachers => Set<Teacher>();
+        public DbSet<Course> Courses => Set<Course>();
+        public DbSet<Student> Students => Set<Student>();
+        public DbSet<StudentCourse> StudentCourses => Set<StudentCourse>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+    }
+}
