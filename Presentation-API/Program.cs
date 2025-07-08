c7a405bff5dbf2f2e792e72f7bf85554c84065e4
Mohammad Mehdi Khodabande <mmahdikhodabande@gmail.com>
2025-07-08 10:13:59 +0330

    Update project to .NET 9.0 with new configurations
    - Target .NET 9.0 in `Presentation-API.csproj` and add package references for ASP.NET Core, Entity Framework Core, and Swagger.
    - Introduce `Presentation-API.http` for API host address and sample GET request.
    - Modify `Program.cs` to set up the application builder, configure SQL Server, and implement database seeding.
    - Update `launchSettings.json` for HTTP and HTTPS profiles with environment variables.
    - Create `appsettings.Development.json` for development logging configuration.
    - Modify `appsettings.json` to include SQL Server connection string and logging settings.

---
+++ Presentation-API/Program.cs
@@ -0,0 +1,62 @@
+﻿using Infra;
+using Infra.Persistence;
+using Infra.Persistence.Seed;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// DI برای ریپوزیتوری‌ها و سرویس‌ها
+builder.Services.AddInfrastructure(); 
+
+// سایر سرویس‌ها...
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+
+    context.Students.RemoveRange(context.Students);
+    await context.SaveChangesAsync();
+
+    await Seeder.SeedAsync(context);
+}
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/swagger");
+    return Task.CompletedTask;
+});
+
+app.Run();
