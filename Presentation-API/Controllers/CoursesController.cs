c29b08762cbffec45cc55ec5d4842b683b0beb02
Mohammad Mehdi Khodabande <mmahdikhodabande@gmail.com>
2025-07-08 10:13:30 +0330

    Add CoursesController for course management API
    Introduces a new `CoursesController` in the `Presentation_API.Controllers` namespace. This controller is set up to handle API requests at the "api/courses" endpoint. It includes a constructor that injects an `ICourseReadService` and features a `Get` method for retrieving and sorting a list of courses based on search parameters.

---
+++ Presentation-API/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseReadService _courseService;
+
+        public CoursesController(ICourseReadService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        /// <summary>
+        /// دریافت لیست دوره‌ها با امکان جستجو و مرتب‌سازی
+        /// </summary>
+        /// <param name="search">کلمه جستجو در عنوان یا توضیح</param>
+        /// <param name="sortBy">معیار مرتب‌سازی: rank (پیش‌فرض)، title، duration</param>
+        /// <returns>لیست دوره‌ها</returns>
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string? search, [FromQuery] string? sortBy = "rank")
+        {
+            var courses = await _courseService.GetCourseAsync(search, sortBy);
+            return Ok(courses);
+        }
+    }
+}
