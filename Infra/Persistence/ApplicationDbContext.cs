7494fe87f448f9b71e99a261f30acd5243eb612a
Mohammad Mehdi Khodabande <mmahdikhodabande@gmail.com>
2025-07-08 10:12:46 +0330

    Add ApplicationDbContext for database management
    Introduces the `ApplicationDbContext` class in the `Infra.Persistence` namespace, inheriting from `DbContext`. This class includes `DbSet` properties for `Subjects`, `Sources`, `Teachers`, `Courses`, `Students`, and `StudentCourses`, enabling CRUD operations. The `OnModelCreating` method is overridden to apply entity configurations from the assembly, facilitating proper entity mapping and relationships in the database.

---
+++ Infra/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Subject> Subjects => Set<Subject>();
+        public DbSet<Source> Sources => Set<Source>();
+        public DbSet<Teacher> Teachers => Set<Teacher>();
+        public DbSet<Course> Courses => Set<Course>();
+        public DbSet<Student> Students => Set<Student>();
+        public DbSet<StudentCourse> StudentCourses => Set<StudentCourse>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+    }
+}
