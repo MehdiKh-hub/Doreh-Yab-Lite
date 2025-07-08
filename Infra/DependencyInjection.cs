2500233cb9f04faff2dc683c4ee4bba19622ebfd
Mohammad Mehdi Khodabande <mmahdikhodabande@gmail.com>
2025-07-08 10:12:57 +0330

    Add Dependency Injection and update project configuration
    Updated `DependencyInjection.cs` to include new using directives and added a static class for service registration. Modified `Infra.csproj` to target .NET 9.0, enable implicit usings, and added necessary package and project references for Entity Framework Core.

---
+++ Infra/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.Ripository;
+using Application.Interfaces.Services;
+using Application.Services;
+using Infra.Persistence.Ripositories;
+using Infra.Persistence.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddScoped<IStudentService,StudentService>();
+            services.AddScoped<ICourseReadService, CourseReadService>();
+
+            return services;
+        }
+    }
+}
