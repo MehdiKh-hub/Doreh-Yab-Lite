39244e7cb6c1ddb238a362e3c420dee64f6005ca
Mohammad Mehdi Khodabande <mmahdikhodabande@gmail.com>
2025-07-27 13:42:25 +0330

    Add CourseRankingDto class for course ranking data
    Introduced a new `CourseRankingDto` class in the `Application.DTOs` namespace. This class contains properties such as `CourseId`, `CourseTitle`, `TeacherName`, `SourceName`, `CourseRank`, `TeacherRank`, `SourceRank`, `TotalRank`, `DurationInHours`, and an optional `Description` to encapsulate data related to course rankings.

---
+++ Application/DTOs/CourseRankingDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class CourseRankingDto
+    {
+        public int CourseId { get; set; }
+        public string CourseTitle { get; set; }
+        public string TeacherName { get; set; }
+        public string SourceName { get; set; }
+        public int CourseRank { get; set; }
+        public int TeacherRank { get; set; }
+        public int SourceRank { get; set; }
+        public int TotalRank { get; set; }
+        public int DurationInHours { get; set; }
+        public string? Description { get; set; }
+    }
+}
